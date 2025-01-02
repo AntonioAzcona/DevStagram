bde2ef15214d7e53c2e60289b728e769d508b92c
AntonioAzcona <tom.ramms.15@gmail.com>
2025-01-01 23:11:52 -0600

    Add initial files

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Nosotros
+@endsection
+
+@section('contenido')
+    Contenido de la página de nosotros
+@endsection
---
+++ resources/views/tienda.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Tienda virtual
+@endsection
+
+@section('contenido')
+    Contenido de la tienda
+@endsection
