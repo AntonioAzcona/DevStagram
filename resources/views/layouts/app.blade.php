fce4d7068757bcdbe4bbc6db191d4ff4a5b57533
AntonioAzcona <tom.ramms.15@gmail.com>
2025-01-06 12:08:34 -0600

    RegisterDesingComplete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>DevStagram - @yield('titulo')</title>
         @vite('resources/css/app.css')
+        <title>DevStagram - @yield('titulo')</title>
+        @vite('resources/js/app.js')
     </head>
     <body class="bg-gray-100">
         <header class="p-5 border-b bg-white shadow">
@@ -13,7 +14,7 @@
 
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
                 </nav>
             </div>
         </header>
